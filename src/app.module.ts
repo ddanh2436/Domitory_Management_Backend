48542c56f2663a65f8ca22d8c34506a81ae8fb62
Đào Duy Anh <131458303+Daoduyanh2412@users.noreply.github.com>
2026-06-11 10:07:59 +0700

    Connect to Mongodb Atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,25 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 import { RoomsModule } from './rooms/rooms.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/dormitory_db'),
-    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
     AuthModule,
+    UsersModule,
     RoomsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
